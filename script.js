65c61ce5adc1077241b84b8b0d9c2011febf1978
Elijah <piperelijah@gmail.com>
2019-12-20 10:49:17 -0700

    README.md

---
+++ script.js
@@ -0,0 +1,4 @@
+//******************************************
+// Rock-Paper-Scissors game JavaScript file
+// This is holds the logic of the game itself.'
+//******************************************
